92249414eaa28a9929fb64f47531ccdbe324032c
Enrico Dainelli <enrico_mail@pm.me>
2023-10-26 18:18:18 +0000

    Game development with TDD

---
+++ longest_word/game.py
@@ -0,0 +1,19 @@
+import random
+import string
+class Game:
+    def __init__(self) -> list:
+        """Attribute a random grid to size 9"""
+        self.grid = []
+        for _ in range(9):
+            self.grid.append(random.choice(string.ascii_uppercase))
+
+    def is_valid(self, word):
+        if not word:
+            return False
+        letters = self.grid.copy() # Consume letters from the grid
+        for letter in word:
+            if letter in letters:
+                letters.remove(letter)
+            else:
+                return False
+        return True
